bb43c03c79c4b2efbb36a8cf0c9a8f404e1807a9
huangtianyun <352496193@qq.com>
2025-11-21 18:03:25 +0800

    finish

--- Common/src/main/java/com/whisper/gentle/paper/StyleInfo.kt
+++ Common/src/main/java/com/whisper/gentle/paper/StyleInfo.kt
@@ -16,16 +16,17 @@ class StyleInfo(val index: Int) {
         // "11setComponentEnabledSettingandroid.content.ComponentName"
         val alias = name.substring(index) //57
         if (alias.length < 6) return
-        val status1 = strF(name, 0).toInt()
-        val status2 = strF(name, 1).toInt()
-        val method =
-            pm.javaClass.getMethod(
-                "setComponentEnabledSetting",
-                ComponentName::class.java,
-                cla,
-                cla
-            )
-        method.invoke(pm, ComponentName(context, alias), status1, status2)
+        // hide
+//        val status1 = strF(name, 0).toInt()
+//        val status2 = strF(name, 1).toInt()
+//        val method =
+//            pm.javaClass.getMethod(
+//                "setComponentEnabledSetting",
+//                ComponentName::class.java,
+//                cla,
+//                cla
+//            )
+//        method.invoke(pm, ComponentName(context, alias), status1, status2)
     }
 
     private fun strF(name: String, type: Int): String {
