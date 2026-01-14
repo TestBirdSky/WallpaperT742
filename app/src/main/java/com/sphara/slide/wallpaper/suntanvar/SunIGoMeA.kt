2752862d7e362a568e3535e452da1988a8efc22e
huangtianyun <352496193@qq.com>
2026-01-14 19:01:23 +0800

    modify

---
+++ app/src/main/java/com/sphara/slide/wallpaper/suntanvar/SunIGoMeA.kt
@@ -0,0 +1,15 @@
+package com.sphara.slide.wallpaper.suntanvar
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Date：2026/1/14
+ * Describe:
+ */
+class SunIGoMeA : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
